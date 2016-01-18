91ec4cbe5c3adb6da8b0cf9f97af057ba3588000
Stefan Heitmueller <s.heitmueller@packetwerk.de>
2016-01-18 10:18:38 +0100

    initial commit

---
+++ scripts/after-install.sh
@@ -0,0 +1,1 @@
+update-alternatives --install /etc/lightdm/lightdm-gtk-greeter.conf lightdm-gtk-greeter-config-derivative /etc/lightdm/lightdm-gtk-greeter-numix.conf 30
---
+++ scripts/before-remove.sh
@@ -0,0 +1,1 @@
+update-alternatives --remove lightdm-gtk-greeter-config-derivative /etc/lightdm/lightdm-gtk-greeter-numix.conf
